e92f3dddf0706ddeb32f1a9271551cda43ba5fd2
David Silva <Davidslv@users.noreply.github.com>
2025-10-20 20:39:45 +0100

    Update installation guide for Ruby on Rails v8
    Just updating the documentation as there's no `bundle` inside the `bin` folder in Ruby on Rails 8.
    
    I actually think the intro (which is not in use) is accurate.

--- src/app/(docs)/docs/installation/framework-guides/ruby-on-rails.tsx
+++ src/app/(docs)/docs/installation/framework-guides/ruby-on-rails.tsx
@@ -11,7 +11,7 @@ export let tile: Tile = {
 
 export let page: Page = {
   title: "Install Tailwind CSS with Ruby on Rails",
-  description: "Setting up Tailwind CSS in Ruby on Rails v7+ project.",
+  description: "Setting up Tailwind CSS in Ruby on Rails v8+ project.",
 
   // NOTE: This intro is not used currently but is here for reference as we'll want to bring it back once the rails gem is updated for a stable v4 release.
   intro: (
@@ -39,7 +39,7 @@ export let steps: Step[] = [
       name: "Terminal",
       lang: "shell",
       code: shell`
-        rails new my-project
+        rails new my-project --css tailwind
         cd my-project
       `,
     },
@@ -57,8 +57,8 @@ export let steps: Step[] = [
       name: "Terminal",
       lang: "shell",
       code: shell`
-        ./bin/bundle add tailwindcss-ruby
-        ./bin/bundle add tailwindcss-rails
+        bundle add tailwindcss-ruby
+        bundle add tailwindcss-rails
         ./bin/rails tailwindcss:install
       `,
     },
